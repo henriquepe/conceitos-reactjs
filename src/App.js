29e8d122ab71e0f915fa91643016d8f3f7966908
Henrique <henrique.p.espindola@gmail.com>
2020-04-09 21:12:29 -0300

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+
+  useEffect(()=> {
+    api.get('/repositories').then(res =>{
+      setRepositories(res.data);
+    });
+
+  }, []);
+
+  /*axios mock adapter = consegue fazer a aplicacao identificar uma chamada api e retornar outra resposta, o teste funciona como um ficticio backend
+  */
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('/repositories', {
+      title: "Front-end Developer",
+      url: "http://qualquercoisa.com/henriquepe",
+      techs: ["React", "Angular", "Vue.js"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    //handleRemoveRepository(repository.id)}
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id))
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
@@ -29,3 +60,4 @@ function App() {
 }
 
 export default App;
+
